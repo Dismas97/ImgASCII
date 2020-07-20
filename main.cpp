f35781ef59e9aa8964d6af984fa5f766df201197
Francisco <rosasfran.97@gmail.com>
2020-07-20 16:23:33 -0300

    Con 10/70/96 caracteres

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 using namespace std;
 using namespace sf;
 
+/**
+@author Dismas
+**/
 
 void limpiarBuffer(){
     while (getchar() != '\n' && getchar() != EOF);
@@ -16,20 +19,29 @@ void setTam(Image img,int *ancho, int *alto){
 }
 
 char getCharMap(string mapa, Color pixel){
-    int tam = mapa.length();
-    int escalado = (pixel.b*0.0722+pixel.r*0.2126 + pixel.g*0.7152)*tam;
+    int escalado = (pixel.b*0.0722+pixel.r*0.2126 + pixel.g*0.7152)*mapa.length();
     escalado/=255;
-    return mapa[tam-escalado];
+    return mapa[escalado];
 }
 
 int main(){
     int ancho, alto;
     Image imagen;
     Color pixel;
-    string pathImagen,escribir,pathArchivo, mapa=" .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+    string pathImagen,escribir,pathArchivo;
+    string mapa;
     ofstream archivo;
+    cout<<"Cantidad de caracteres:\n(1) 10 (2) 70 (3) 96\n";
+    int cant;
+    cin>>cant;
+    switch(cant){
+        case 1: mapa = "@#*+=;:-. "; break;
+        case 2: mapa = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "; break;
+        case 3: mapa = "@MBHENR#KWXDFPQASUZbdehx*8Gm&04LOVYkpq5Tagns69owz$CIu23Jcfry%1v7l+it[] {}?j|()=~!-/<>\\\"^_';,:`. "; break;
+        default: mapa = "@MBHENR#KWXDFPQASUZbdehx*8Gm&04LOVYkpq5Tagns69owz$CIu23Jcfry%1v7l+it[] {}?j|()=~!-/<>\\\"^_';,:`. ";
+    }
 
-    cout<< "Path imagen"<<endl;
+    cout<<"Path imagen"<<endl;
     cin >> pathImagen;
 
     imagen.loadFromFile(pathImagen);
