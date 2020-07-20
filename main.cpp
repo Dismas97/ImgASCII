6f02c45ba6c866345d1810920e727b016b3cc4ab
Francisco <rosasfran.97@gmail.com>
2020-07-20 05:07:47 -0300

    Terminanding

--- main.cpp
+++ main.cpp
@@ -5,20 +5,46 @@
 void limpiarBuffer(){
     while (getchar() != '\n' && getchar() != EOF);
 }
-
 using namespace std;
 
+sf::Image getImagen(string path){
+    sf::Image imagen;
+    imagen.loadFromFile(path);
+    return imagen;
+}
+
+void setTam(sf::Image img,int *ancho, int *alto){
+    (*ancho) = img.getSize().x;
+    (*alto) = img.getSize().y;
+}
+
 int main(){
-    int ancho,altura;
+    int ancho, alto, aux;
+    string pathImagen,pathArchivo, escribir = "", mapa=" .,:;ox%#@";
+    ofstream archivo;
     sf::Image imagen;
     sf::Color pixel;
-    string pathImagen;
-    cout<< "Nombre imagen"<<endl;
+    cout<< "Path imagen"<<endl;
     cin >> pathImagen;
-    imagen.loadFromFile(pathImagen);
-    ancho = imagen.getSize().x;
-    altura = imagen.getSize().y;
-    cout<< ancho<<'\n'<<altura<<"\nPresione cualquier tecla para continuar"<<endl;
+    limpiarBuffer();
+    cout<< "Path Texto"<<endl;
+    cin>>pathArchivo;
+    imagen = getImagen(pathImagen);
+    setTam(imagen,&ancho,&alto);
+
+    for(int y=0; y<alto; y++){
+        for(int x=0; x<ancho; x++){
+            pixel = imagen.getPixel(x,y);
+            aux = (pixel.r + pixel.g + pixel.b)*10;
+            aux/=765;
+            escribir+=mapa[10-aux];
+        }
+        escribir+='\n';
+    }
+    archivo.open(pathArchivo);
+    archivo << escribir;
+    archivo.close();
+    cout<<"Archivo generado con exito\n Presione cualquier tecla para salir"<<endl;
     limpiarBuffer();
     getchar();
     return 0;
