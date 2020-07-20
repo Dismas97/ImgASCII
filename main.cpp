67ab8f3af12c3cfec61766b81a7e9c55209430e4
Francisco <rosasfran.97@gmail.com>
2020-07-20 02:46:25 -0300

    Obtengo altura y ancho de la imagen

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <fstream>
 
+using namespace std;
+
+int main(){
+  int ancho,altura;
+  sf::Image imagen;
+  sf::Color pixel;
+  imagen.loadFromFile("ej1.jpg");
+  ancho = imagen.getSize().x;
+  altura = imagen.getSize().y;
+  cout<< ancho <<endl;
+  cout<< altura <<endl;
+  return 0;
+}
