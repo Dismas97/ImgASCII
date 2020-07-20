c239054ad89683d142689fb83aaf0af6ac343103
Francisco <rosasfran.97@gmail.com>
2020-07-20 03:40:24 -0300

    .

--- main.cpp
+++ main.cpp
@@ -2,16 +2,24 @@
 #include <SFML/Graphics.hpp>
 #include <fstream>
 
+void limpiarBuffer(){
+    while (getchar() != '\n' && getchar() != EOF);
+}
+
 using namespace std;
 
 int main(){
-  int ancho,altura;
-  sf::Image imagen;
-  sf::Color pixel;
-  imagen.loadFromFile("ej1.jpg");
-  ancho = imagen.getSize().x;
-  altura = imagen.getSize().y;
-  cout<< ancho <<endl;
-  cout<< altura <<endl;
-  return 0;
+    int ancho,altura;
+    sf::Image imagen;
+    sf::Color pixel;
+    string pathImagen;
+    cout<< "Nombre imagen"<<endl;
+    cin >> pathImagen;
+    imagen.loadFromFile(pathImagen);
+    ancho = imagen.getSize().x;
+    altura = imagen.getSize().y;
+    cout<< ancho<<'\n'<<altura<<"\nPresione cualquier tecla para continuar"<<endl;
+    limpiarBuffer();
+    getchar();
+    return 0;
 }
